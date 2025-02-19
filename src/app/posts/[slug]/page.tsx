83c8a8db097344f48c7530ef6b1c6adbfa98cda3
NanaGlutamate <30829895+NanaGlutamate@users.noreply.github.com>
2025-02-20 02:24:29 +0800

    fix: numbered list no number; page title and icon

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import * as fs from "fs/promises"
 import type { MetaData, Page, PageNodeRoot } from "@/lib/pagetype"
 import { PageRoot } from "@/lib/render"
+import * as Next from 'next'
 
 export async function generateStaticParams() {
     const metaData: MetaData = JSON.parse(await fs.readFile('./.notion_out/__meta_data__.json', 'utf-8'))
@@ -23,4 +24,23 @@ export default async function Page({
     return (
         <PageRoot metaData={metaData} pageInfo={pageInfo} root={root} />
     )
+}
+
+export async function generateMetadata({
+    params,
+}: {
+    params: Promise<{ slug: string }>
+}): Promise<Next.Metadata> {
+    if (typeof params == 'undefined') {
+        throw new Error('no param')
+    }
+    const { slug } = await params
+    const metaData: MetaData = JSON.parse(await fs.readFile('./.notion_out/__meta_data__.json', 'utf-8'))
+    const id = metaData.slug_to_id[slug] ?? (() => { throw new Error('no id') })()
+    const pageInfo = metaData.id_to_data[id] ?? (() => { throw new Error('no page info') })()
+    return {
+        title: "文章 / " + pageInfo.Title,
+        keywords: pageInfo.Tag,
+        icons: '/favicon.ico',
+    }
 }
