16e1ece2b1c88a05aad8f74be618ec6a7f16743c
NanaGlutamate <30829895+NanaGlutamate@users.noreply.github.com>
2026-07-27 02:22:10 +0800

    refactor: 依赖库的安全升级

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,4 +1,5 @@
-import { defineCollection, z } from 'astro:content'
+import { defineCollection } from 'astro:content'
+import { z } from 'astro/zod'
 import { execSync } from 'child_process'
 
 const posts = defineCollection({
